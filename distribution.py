47c3979faf887e51e77fe18206e893532b203f6f
ryankynor <ryankynor@hanovernorwichschools.org>
2015-10-26 21:35:04 -0400

    Updated from Brython Server: 10/26/2015 9:35:04 PM

--- distribution.py
+++ distribution.py
@@ -50,9 +50,7 @@ stext = sorted(ltext)
 
 textlength = int(textlen)
 for stext in ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    for c results:
-        if c == count:
-            print(c)
+    dis.append(ltext.count(stext))
 
 """
 highest = []
